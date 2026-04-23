0a6fb431e99c7b7ca7b90070de4bfd15ca6b202e
jacob colton <jacob.wayne.colton@gmail.com>
2026-04-23 03:42:52 -0400

    Completed the model successfully

--- generate_data.py
+++ generate_data.py
@@ -1,15 +1,15 @@
-# imports
 import random
 
-# function to generate inputs and labels
+# generating the data
 def generate(size):
     X = []
     y = []
-   for i in range(size) 
-        # appending a random int between 0 and 200 to X
-        X.append(random.randint(0,200))
-        # appending a random int between 0 and 10 to y
-        y.append(random.randint(0,10))
+    for i in range(size):
+        x = random.randint(0, 200)
+        noise = random.uniform(-2, 2)   # small randomness
+        label = 0.5 * x + 3 + noise
 
-# returning the inputs and corresponding labels as a list
-    return X,y
+        X.append(x)
+        y.append(label)
+
+    return X, y
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import numpy
 import generate_data
 
 # global variables
-EPOCHS = 50
-LEARNING_RATE = 0.1 # function for the model to make a prediction
+EPOCHS = 5000
+LEARNING_RATE = 0.1
+DATA_SIZE = 2000
+
 def predict(input, weight,bias):
     prediction = input * weight + bias
     return prediction
@@ -16,38 +18,61 @@ def calculate_error(prediction,true_label):
     return error
 
 # calcuate loss using Mean Squared Error (MSE) loss function
-def MSE(data_size, y, y_hat):
+def MSE(y, y_hat):
     # calculating mean squared error
-    L = (1/(2 * data_size)) * numpy.sum([calculate_error(y[i], y_hat[i]) for i in range(data_size))**2
+    L = (1/(2 * DATA_SIZE)) * numpy.sum([calculate_error(y[i], y_hat[i])**2 for i in range(DATA_SIZE)])
     return L
 
 # update the weight and bias
-def update_params():
+def update_params(x,y,w,b):
     a = LEARNING_RATE    
-    w = w - a * ((-1/n) * numpy.sum())
+    dw = ((-1/DATA_SIZE) * numpy.sum([x[i]*(y[i] - (w*x[i] +b)) for i in range(DATA_SIZE)]))
+    db = ((-1/DATA_SIZE) * numpy.sum([(y[i] - (w * x[i] + b)) for i in range(DATA_SIZE)] ))
 
-
+    w = w - a * dw
+    b = b - a * db
+    return w,b
 
 
 # for the main execution
 def main():
     # generating the data 
-    X, y = generate_data.generate(data_size) 
+    X, y = generate_data.generate(DATA_SIZE)
+    # Converting to numpy arrays so i can normalize the data
+    X, y = numpy.array(X), numpy.array(y)
+    # normalizing the data
+    X = (X - X.mean()) / X.std()
+    y = (y - y.mean()) / y.std()
     y_hat = []
 
     # setting the weight and the bias to default values
     weight = 0.5
     bias = -40
 
+    # making predictions before training
+    for i in range(20):
+        print(f"Prediction : {predict(X[i],weight,bias)}, Answer = {y[i]}")
+
     # looping over the whole data set
     for epoch in range(EPOCHS):
         # creating the training loop over the whole data set
-        for i in range(data_size):
+        y_hat = []
+        for i in range(DATA_SIZE):
             # the model makes a prediction
-            y_hat[i] = predict(X[i], y[i]) 
+            y_hat.append(predict(X[i],weight,  bias))
 
         # calculate loss
-        L = MSE(data_size, y, y_hat)
+        L = MSE(y, y_hat)
+        
+        print(weight,bias)
+        # update the parameters
+        weight,bias = update_params(X,y,weight,bias)
+        print(weight,bias)
+
+    # making predictions after model training
+    for i in range(20):
+        print(f"Prediction post-training : {predict(X[i],weight,bias)}, Answer = {y[i]}")
+    
 
 
             
